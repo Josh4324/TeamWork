2b6c3a4c3aec76b202333d61bdc73b8d0624aa0f
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-04-27 16:31:27 +0100

    signup validation added

---
+++ middleware/validation.js
@@ -0,0 +1,50 @@
+const {
+    body,
+    validationResult
+} = require('express-validator');
+
+exports.signUpValidationRules = () => {
+    console.log("val");
+    return [
+        // username must be an email
+        body("firstName").notEmpty().isLength({
+            min: 3
+        }).isAlpha().trim().escape().withMessage("Name must have more than 3 characters"),
+        body("lastName").notEmpty().isLength({
+            min: 3
+        }).isAlpha().trim().escape().withMessage("Name must have more than 3 characters"),
+        body("email").notEmpty().isEmail().normalizeEmail().withMessage("Email is required"),
+        body("password").notEmpty().isLength({
+            min: 5
+        }).withMessage("Password must have at least 5 characters"),
+        body("jobRole").notEmpty().isAlpha(),
+        body("gender").notEmpty(),
+        body("department").notEmpty(),
+        body("address").notEmpty()
+    ]
+}
+
+exports.userValidation = (req, res, next) => {
+    console.log("checking");
+    try {
+        console.log("err");
+        const errors = validationResult(req)
+        if (errors.isEmpty()) {
+            return next()
+        }
+        const extractedErrors = []
+        errors.array().map(err => extractedErrors.push({
+            [err.param]: err.msg
+        }))
+
+        return res.status(422).json({
+            errors: extractedErrors,
+        })
+
+    } catch {
+        res.status(401).json({
+            error: "Unauthorized",
+            status: "error"
+        })
+    }
+}
--- routes/employee.js
+++ routes/employee.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const validation = require("../middleware/validation");
+
 
 
 const userController = require('../controllers/employee')
 
-router.post('/create-user', auth.authorizationsignup, userController.signup);
+router.post('/create-user', auth.authorizationsignup, validation.signUpValidationRules(), validation.userValidation, userController.signup);
 router.post('/signin', userController.login);
 
 
